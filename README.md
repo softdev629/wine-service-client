2152d3d3f3d5c9cf75592e13ac0e7ac70c01bf9a
Leo Kott <121113067+violetstar701@users.noreply.github.com>
2023-02-26 19:01:41 -0500

    wine-service-client

---
+++ README.md
@@ -0,0 +1,83 @@
+# Wine Service(ChatGPT) Client
+
+> This Chatbot helps customers to select proper wine as they wish.
+
+## Table of Contents
+
+- [General Info](#general-information)
+- [Technologies Used](#technologies-used)
+- [Features](#features)
+- [Screenshots](#screenshots)
+- [Setup](#setup)
+- [Project Status](#project-status)
+- [Room for Improvement](#room-for-improvement)
+- [Acknowledgements](#acknowledgements)
+- [Contact](#contact)
+
+## General Information
+
+- This is customer wine service project.
+- Has to be combined with server project
+- Autochat with customers by using wine information of shop,
+- The reason why I undertook it is because of strong interest on OpenAI,
+
+## Technologies Used
+
+- Tech 1 - OpenAI API
+- Tech 2 - Vanilla
+- Tech 3 - JavaScript
+
+## Features
+
+List the ready features here:
+
+- Autochat
+- Simple Chat UI
+- Imitation of ChatGPT Sample
+
+## Screenshots
+
+![Chat screenshot](./img/screenshot-1.png)
+![Wineinfo screenshot](./img/screenshot-2.png)
+![Finetuning screenshot](./img/screenshot-3.png)
+
+<!-- If you have screenshots you'd like to share, include them here. -->
+
+## Setup
+
+Requires Node.js to be installed.
+Need .env file which OpenAI API_KEY is stored. That is located in /server directory.
+
+Go into /client direcotry and run `npm run dev` CLI command.
+Go into /server directory and run `npm run server` CLI command.
+
+## Project Status
+
+Project is: _in progress_
+
+## Room for Improvement
+
+These are areas i believe need improvement / could be improved. Also added TODOs for future development.
+
+Room for improvement:
+
+- AI theory
+- OpenAI API integration
+- Node.js modules
+
+To do:
+
+- Flexible UI
+- Validation
+
+## Acknowledgements
+
+Give credit here.
+
+- This project was inspired by Valentyn.
+- This project was based on [this tutorial](https://platform.openai.com/docs/).
+- Many thanks to Valentyn.
+
+## Contact
+
+Created by [@violetstar701](https://) - feel free to contact me!
---
+++ script.js
@@ -0,0 +1,122 @@
+import bot from "./assets/bot.png";
+import user from "./assets/user.png";
+import axios from "axios";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+// let chatHistory =
+//   "The following is a conversation with a wine shop virtual assistant and a customer. The virtual assistant helps customers to get proper wine name as they wish by telling exact & clear wine name plus vintage.";
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+      ++index;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class="profile">
+          <img
+            src="${isAi ? bot : user}"
+            alt="${isAi ? "bot" : "user"}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+  `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  const prompt = data.get("prompt").trim();
+
+  if (prompt === "") {
+    form.reset();
+    return;
+  }
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, "Customer: " + prompt);
+
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  // fetch data from server -> bot's response
+  // const response = await axios.post("http://localhost:5000", {
+  //   prompt: data.get("prompt"),
+  // });
+  const formData = new FormData();
+  formData.append("prompt", data.get("prompt"));
+  const response = await fetch("http://localhost:5000/mad", {
+    method: "POST",
+    body: formData,
+  });
+  console.log(response);
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.answer.trim();
+
+    typeText(messageDiv, "Assistant: " + parsedData);
+    // chatHistory += prompt + parsedData;
+  } else {
+    const err = await response.json();
+
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
